59f537c654e42094e8369a43d1bc3abd07416eec
Eric Marques <erc_m@hotmail.com>
2021-06-24 23:40:53 -0300

    add migrations

---
+++ database/migrations/2021_06_25_011835_create_table_real_state.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableRealState extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('real_state', function (Blueprint $table) {
+            $table->increments('id');
+            //Relacionamento
+            $table->unsignedInteger('user_id');
+            //Imóvel
+            $table->string('title');
+            $table->string('description');
+            $table->text('content');
+            $table->float('price', 10, 2);
+            $table->integer('bathrooms');
+            $table->integer('badrooms');
+            $table->integer('garage');
+            $table->integer('property_area');
+            $table->integer('total_property_area');
+            $table->string('slug');
+
+            $table->timestamps();
+
+            //Referência do relacionamento
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('real_state');
+    }
+}
