a16cdfb2fbcb1785e63696a97d3a9c3317ac5b86
Eric Marques <erc_m@hotmail.com>
2021-07-03 23:04:46 -0300

    add category model

--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,7 @@ Route::prefix('v1')->namespace('Api')->group(function(){
     Route::name('users.')->group(function(){
         Route::resource('users', 'UserController');
     });
+    Route::name('categories.')->group(function(){
+        Route::resource('categories', 'CategoryController');
+    });
 });
